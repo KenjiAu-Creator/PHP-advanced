fb04a02fcb1015a48e63d04eecf97348867a4cbc
Kenji <kenji.au9@gmail.com>
2020-10-05 14:56:09 -0600

    Added JS DOM output of search results;

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,42 @@
 // Test that this file is loaded properly!
 // alert("Im here");
 
-const snackSearchForm = document.getElementById( 'search-form' );
-const snackSearchInput = document.getElementById( 'search-input' );
-const snackResults = document.getElementById( 'search-results' );
+const snackSearchForm = document.getElementById("search-form");
+const snackSearchInput = document.getElementById("search-input");
+const snackResults = document.getElementById("search-results");
 
-/** 
+/**
  * Listen for a submit
  */
 
- snackSearchForm.addEventListener('submit', event => {
+snackSearchForm.addEventListener("submit", (event) => {
   // Stop the form from submitting the traditional way.
   event.preventDefault();
   const results = false;
   // Attempt a fetch for results.
-  fetch("http://localhost:3000/api/snacks.php")
-    .then( response => response.json() )
-    .then( data => {
-      console.log( data );
+  fetch(`http://localhost:3000/api/snacks.php?search=${snackSearchInput.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      for (let snack of data) {
+        // Create elements.
+        const snackLI = document.createElement("LI");
+
+        // Populate the elements.
+        snackLI.innerHTML = `
+        <h3>${snack[0]}</h3>
+        <dl>
+          <dt>Type</dt>
+          <dd>${snack[1]}</dd>
+          <dt>Price</dt>
+          <dd>$${parseFloat(snack[2]).toFixed(2)}</dd>
+          <dt>Calories</dt>
+          <dd>${snack[3]}</dd>
+        </dl>
+        `
+        // Append the elements to the list in our webpage.
+        snackResults.append(snackLI);
+      };
+      // Append the elements to the list in our webpage.
     });
- });
+});
