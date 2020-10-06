ba84ec87d2d1b4be098a321350aee46796724f17
Kenji <kenji.au9@gmail.com>
2020-10-06 10:12:57 -0600

    Added in search component base;

--- js/react-example.js
+++ js/react-example.js
@@ -3,4 +3,23 @@
 // Grab our React "Root" element.
 const reactRoot = document.getElementById( 'react-root' );
 
-ReactDOM.render( <p>Hello, World!</p>, reactRoot );
+// Search Form component
+const SearchForm = props => {
+  return (
+    <div>
+      <h2>Snack Search Form</h2>
+      <form>
+        <label htmlFor="search">
+          Enter a Search Term:
+          <input id="search" type="search" />
+        </label>
+        <input type="submit" value="Search Snacks" />
+      </form>
+      <h3>Snack Search Results</h3>
+      <ul></ul>
+    </div>
+  )
+}
+
+// Render into the real DOM.
+ReactDOM.render( <SearchForm />, reactRoot );
